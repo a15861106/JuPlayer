657bc9f6eebe8eed57164b626e3cac5810ce4188
Juwan <JuwanZhu@163.com>
2015-09-30 22:49:21 +0800

    update UI and add choose fie function.

--- app/src/main/java/com/juwan/juplayer/MainActivity.java
+++ app/src/main/java/com/juwan/juplayer/MainActivity.java
@@ -1,14 +1,23 @@
 package com.juwan.juplayer;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.TextView;
+import android.view.View;
+import android.widget.Toast;
 
 import com.juwan.juplayer.jni.Ffmpeg;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final static String TAG = "MainActivity";
+
+    private final static int READ_REQUEST_CODE = 110;
 
     static {
         // load FFmpeg library
@@ -26,7 +35,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ((TextView) findViewById(R.id.hello)).setText("avcodec_version = " + new Ffmpeg().init());
+        // ((TextView) findViewById(R.id.hello)).setText("avcodec_version = " + new Ffmpeg().init());
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
     }
 
     @Override
@@ -48,6 +59,29 @@ public class MainActivity extends AppCompatActivity {
             return true;
         }
 
+        switch (id) {
+            case R.id.action_open_file:
+                Toast.makeText(MainActivity.this, "open file", Toast.LENGTH_SHORT).show();
+                break;
+        }
+
         return super.onOptionsItemSelected(item);
+    }
+
+    public void chooseFile(View v) {
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType("file/*");
+        startActivityForResult(intent, READ_REQUEST_CODE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == READ_REQUEST_CODE && resultCode == Activity.RESULT_OK) {
+            String filePath = data.getDataString();
+            Toast.makeText(MainActivity.this, "file path is " + filePath, Toast.LENGTH_SHORT).show();
+        }
+
     }
 }
